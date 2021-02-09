ecb6ce2d6a95ef4a13d2a7f4eac526937f1429b0
Adiel Ribeiro <contato@nuvym.com>
2021-02-09 14:16:43 -0300

    Hardened - By NUVYM

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', '-#CHANGEME!P@ssw0rd' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
